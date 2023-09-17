306c61a25a88f0a3ddf253d3524b9dbba4e69d54
zlAxel <axel.macias17@hotmail.com>
2023-09-16 23:25:55 -0600

    Se consume API (axios) de categorias

--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -4,24 +4,42 @@ import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 // ! Importamos los datos
-import { categories as categoriesDB } from "../data/categories";
+// import { categories as categoriesDB } from "../data/categories";
 
 export const QuioscoContext = createContext();
+import axios from "axios";
 
 export const QuioscoProvider = ({ children }) => {
 
     // ? Creamos el estado global
-    const [categories, setCategories] = useState(categoriesDB) // * Estado para las categorias
-    const [currentCategory, setCurrentCategory] = useState(categories[0]) // * Estado dinamico para la categoria actual
+    const [categories, setCategories] = useState([]) // * Estado para las categorias
+    const [currentCategory, setCurrentCategory] = useState({}) // * Estado dinamico para la categoria actual
     const [modal, setModal] = useState(false) // * Estado dinamico para el modal
     const [product, setProduct] = useState({}) // * Estado dinamico para el producto
     const [order, setOrder] = useState([]) // * Estado para almacenar todas las ordenes
     const [total, setTotal] = useState(0) // * Estado dinamico para el total de la orden
 
     useEffect(() => {
+        getCategories()
+    }, [])
+    
+
+    useEffect(() => {
         const newTotal = order.reduce(( total, orderState ) => ( orderState.price * orderState.amount ) + total, 0 )
         setTotal( newTotal )
-    }, [order])
+    }, [order]);
+
+    // ? Consumimos categoriesDB asincrona para obtener los datos de la API
+    const getCategories = async () => {
+        try {
+            const { data } = await axios( "http://localhost:8000/api/categorias" )
+            
+            setCategories( data.data )
+            setCurrentCategory( data.data[0] )
+        } catch (error) {
+            console.log( error )
+        }
+    }
 
     // ? Creamos función para manejar el evento click en el boton de ordenar
     const handleClickOrder = ( { category_id, ...product } ) => {
--- src/data/categories.js
+++ src/data/categories.js
@@ -1,33 +1,33 @@
 
-export const categories = [
-    {
-        icon: "cafe",
-        name: "Café",
-        id:1
-      },
-      {
-        icon: "hamburguesa",
-        name: "Hamburguesas",
-        id: 2
-      },
-      {
-        icon: "pizza",
-        name: "Pizzas",
-        id: 3
-      },
-      {
-        icon: "dona",
-        name: "Donas",
-        id: 4
-      },
-      {
-        icon: "pastel",
-        name: "Pasteles",
-        id: 5
-      },
-      {
-        icon: "galletas",
-        name: "Galletas",
-        id: 6
-      }
-]
+// export const categories = [
+//     {
+//         icon: "cafe",
+//         name: "Café",
+//         id:1
+//       },
+//       {
+//         icon: "hamburguesa",
+//         name: "Hamburguesas",
+//         id: 2
+//       },
+//       {
+//         icon: "pizza",
+//         name: "Pizzas",
+//         id: 3
+//       },
+//       {
+//         icon: "dona",
+//         name: "Donas",
+//         id: 4
+//       },
+//       {
+//         icon: "pastel",
+//         name: "Pasteles",
+//         id: 5
+//       },
+//       {
+//         icon: "galletas",
+//         name: "Galletas",
+//         id: 6
+//       }
+// ]
